e7d491801b241dffc2da6939c94859aa7ab4bd07
EnigmaSn <enigmasnape@gmail.com>
2017-03-17 01:06:03 +0300

    1

---
+++ src/markup/components/header/data/data.js
@@ -0,0 +1,19 @@
+var data = {
+  header: {
+    langs: [
+      {
+        name: 'USA Site',
+        url: '/usa'
+      },
+      {
+        name: 'Deutch Site',
+        url: '/de'
+      },
+      {
+        name: 'Switch to English',
+        url: '/en',
+        span: 'Россия: '
+      }
+    ]
+  }
+};
---
+++ src/markup/components/navigation/data/data.js
@@ -0,0 +1,21 @@
+var data = {
+  navigation: {
+    tabs: [
+      {
+        name: 'Возможности',
+        url: '/#',
+        class: 'index'
+      },
+      {
+        name: 'Информация',
+        url: '/#',
+        class: 'info'
+      },
+      {
+        name: 'Привилегии',
+        url: '/#',
+        class: 'privilege'
+      }
+    ]
+  }
+};
